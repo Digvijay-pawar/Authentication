346118caa3dd863677557c794c5728d0e1693345
Digvijay Pawar <113578725+Digvijay-pawar@users.noreply.github.com>
2024-03-18 01:04:53 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ["https://authentication-phi-one.vercel.app/"],
+    origin: ["authentication-client-flax.vercel.app"],
     methods: ["POST", "PUT", "PATCH", "GET"],
     credentials: true
 
