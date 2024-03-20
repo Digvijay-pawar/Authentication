d0e7226332724ebde7cb39eddf553fa01f1cf4aa
Digvijay Pawar <pawardigvijay.rcpit@gmail.com>
2024-03-20 11:04:34 +0530

    Error file updated...

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -10,6 +10,7 @@ const NotFoundPage = () => {
             <path d="M8.523 1.912c.642-1.11 2.175-1.11 2.817 0l5.598 9.688c.69 1.195-.154 2.712-1.409 2.712H4.334c-1.255 0-2.1-1.517-1.409-2.712L8.523 1.912zM8 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm.935 6.546c-.308.533-1.045.533-1.353 0l-3.531-6.094C4.096 4.901 4.516 4 5.23 4h5.54c.714 0 1.134.901.619 1.452l-3.531 6.094zM8.5 11a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
           </svg>
           <h1 className="mt-3">404 - Not Found</h1>
+          
           <p>This page is temporarily unavailable.</p>
           <Link to="/" className="btn btn-primary">Go Back to Home</Link>
         </div>
